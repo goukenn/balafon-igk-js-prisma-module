07aca38a6a31baa9941cd307382b758d79314edb
C.A.D. Bondje Doue <bondje.doue@gmail.com>
2023-12-19 19:23:35 +0100

    update GenSchemaCommand

--- Lib/Classes/System/Console/Commands/GenSchemaCommand.php
+++ Lib/Classes/System/Console/Commands/GenSchemaCommand.php
@@ -4,7 +4,9 @@
 // @date: 20231219 16:07:22
 namespace igk\js\Prisma\System\Console\Commands;
 
+use igk\js\Prisma\PrismaModelBuilder;
 use IGK\System\Console\AppExecCommand;
+use IGK\System\Console\Logger;
 
 ///<summary></summary>
 /**
@@ -12,15 +14,23 @@ use IGK\System\Console\AppExecCommand;
 * @package igk\js\Prisma\System\Console\Commands
 */
 class GenSchemaCommand extends AppExecCommand{
-	var $command="--primas:gen-schema";
+	var $command="--primas:gen-schemas";
 	var $desc="generate the prisma schema from controller db-cache definition";
-	var $category="category";
+	var $category="prisma";
 	// var $options=[];
 	var $usage='controller';
 	public function exec($command, ?string $ctrl=null) { 
 
 		$ctrl = self::GetController($ctrl) ?? igk_die('missing controller');
 
-		
+		$rm = $ctrl::getCachedDataTableDefinition();
+		if (empty($rm)){
+			Logger::warn('failed to get data defintion from schema');
+			igk_exit(-1);
+		}
+		$model = new PrismaModelBuilder;
+		$src = $model->generatePrismaContent($rm); 
+
+		echo $src.PHP_EOL;
 	}
 }
